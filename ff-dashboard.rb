cd80e05fa0a8bead2b6415057a44893e190ccf4d
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2016-08-23 00:10:35 -0400

    Abstract into models

--- ff-dashboard.rb
+++ ff-dashboard.rb
@@ -1,117 +1,20 @@
 require 'sinatra'
-require 'mongo'
-require 'fantasy_football_nerd'
-
-BYE_WEEKS = 4..11
-WEEKS = 1..17
-POSITIONS = ['QB', 'RB', 'WR', 'TE', 'K', 'DEF']
-POSITIONS_WITHOUT_DEF = POSITIONS - ['DEF']
-
-Mongo::Logger.logger.level = ::Logger::FATAL
-mongo = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'fantasy-football')
+require_relative 'models'
 
 get '/' do
   'Hello'
 end
 
 get '/updatedata' do
-  teams_doc = {
-    dt: Time.now,
-    teams: FFNerd.teams.map {|team| team.to_h}
-  }
-
-  mongo[:teams].insert_one(teams_doc)
-
-  games_doc =  {
-    dt: Time.now,
-    games: FFNerd.schedule.map {|game| game.to_h}
-  }
-
-  mongo[:games].insert_one(games_doc)
-
-  players_doc = {
-    dt: Time.now,
-    players: FFNerd.players.map {|player| player.to_h}
-  }
-
-  mongo[:players].insert_one(players_doc)
-
-  BYE_WEEKS.map do |week_num|
-    bye_week_doc = {
-      dt: Time.now,
-      week_num: week_num,
-      teams: FFNerd.byes(week_num).map {|team| team.to_h}
-    }
-
-    mongo[:bye_weeks].insert_one(bye_week_doc)
-  end
-
-  WEEKS.map do |week_num|
-    injury_week_doc = {
-      dt: Time.now,
-      week_num: week_num,
-      players: FFNerd.injuries(week_num).map {|player| player.to_h}
-    }
-
-    mongo[:injury_weeks].insert_one(injury_week_doc)
-  end
-
-  auction_values_doc = {
-    dt: Time.now,
-    players: FFNerd.auction_values.map {|player| player.to_h}
-  }
-
-  mongo[:auction_values].insert_one(auction_values_doc)
-
-  current_week_doc = {
-    dt: Time.now,
-    current_week: FFNerd.current_week
-  }
-
-  mongo[:current_week].insert_one(current_week_doc)
-
-  draft_rankings_doc = {
-    dt: Time.now,
-    players: FFNerd.standard_draft_rankings.map {|player| player.to_h}
-  }
-
-  mongo[:draft_rankings].insert_one(draft_rankings_doc)
-
-  POSITIONS.map do |position|
-    draft_projections_doc = {
-      dt: Time.now,
-      position: position,
-      players: FFNerd.draft_projections(position).map {|player| player.to_h}
-    }
-
-    mongo[:draft_projections].insert_one(draft_projections_doc)
-  end
-
-  WEEKS.map do |week|
-    POSITIONS.map do |position|
-      weekly_rankings_doc = {
-        dt: Time.now,
-        week: week,
-        position: position,
-        players: FFNerd.weekly_rankings(position, week).map {|player| player.to_h}
-      }
-
-      mongo[:weekly_rankings].insert_one(weekly_rankings_doc)
-    end
-  end
-
-  WEEKS.map do |week|
-    POSITIONS_WITHOUT_DEF.map do |position|
-      weekly_projections_doc = {
-        dt: Time.now,
-        week: week,
-        position: position,
-        players: FFNerd.weekly_projections(position, week).map {|player| player.to_h}
-      }
-
-      mongo[:weekly_projections].insert_one(weekly_projections_doc)
-    end
-  end
-
-  'success'
+  FFNerdCache::Teams.update
+  FFNerdCache::Games.update
+  FFNerdCache::Players.update
+  FFNerdCache::ByeWeeks.update
+  FFNerdCache::Injuries.update
+  FFNerdCache::AuctionValues.update
+  FFNerdCache::CurrentWeek.update
+  FFNerdCache::DraftRankings.update
+  FFNerdCache::DraftProjections.update
+  FFNerdCache::WeeklyRankings.update
+  FFNerdCache::WeeklyProjections.update
 end
---
+++ models.rb
@@ -0,0 +1,202 @@
+require 'mongo'
+require 'fantasy_football_nerd'
+
+Mongo::Logger.logger.level = ::Logger::FATAL
+MONGO = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'fantasy-football')
+
+BYE_WEEKS = 4..11
+WEEKS = 1..17
+POSITIONS = ['QB', 'RB', 'WR', 'TE', 'K', 'DEF']
+POSITIONS_WITHOUT_DEF = POSITIONS - ['DEF']
+
+module FFNerdCache
+  def self.get(collection, query = {})
+    MONGO[collection].find(query).sort(ts: -1).limit(1).first.to_h
+  end
+
+  class Teams
+    def self.get
+      FFNerdCache.get(:teams)
+    end
+
+    def self.update
+      teams_doc = {
+        dt: Time.now,
+        teams: FFNerd.teams.map {|team| team.to_h}
+      }
+
+      MONGO[:teams].insert_one(teams_doc)
+    end
+  end
+
+  class Games
+    def self.get
+      FFNerdCache.get(:games)
+    end
+
+    def self.update
+      games_doc =  {
+        dt: Time.now,
+        games: FFNerd.schedule.map {|game| game.to_h}
+      }
+
+      MONGO[:games].insert_one(games_doc)
+    end
+  end
+
+  class Players
+    def self.get
+      FFNerdCache.get(:players)
+    end
+
+    def self.update
+      players_doc = {
+        dt: Time.now,
+        players: FFNerd.players.map {|player| player.to_h}
+      }
+
+      MONGO[:players].insert_one(players_doc)
+    end
+  end
+
+  class ByeWeeks
+    def self.get(query)
+      FFNerdCache.get(:bye_weeks, query)
+    end
+
+    def self.update
+      BYE_WEEKS.map do |week_num|
+        bye_week_doc = {
+          dt: Time.now,
+          week_num: week_num,
+          teams: FFNerd.byes(week_num).map {|team| team.to_h}
+        }
+
+        MONGO[:bye_weeks].insert_one(bye_week_doc)
+      end
+    end
+  end
+
+  class Injuries
+    def self.get(query)
+      FFNerdCache.get(:injuries, query)
+    end
+
+    def self.update
+      WEEKS.map do |week_num|
+        injuries_doc = {
+          dt: Time.now,
+          week_num: week_num,
+          players: FFNerd.injuries(week_num).map {|player| player.to_h}
+        }
+
+        MONGO[:injuries].insert_one(injuries_doc)
+      end
+    end
+  end
+
+  class AuctionValues
+    def self.get
+      FFNerdCache.get(:auction_values)
+    end
+
+    def self.update
+      auction_values_doc = {
+        dt: Time.now,
+        players: FFNerd.auction_values.map {|player| player.to_h}
+      }
+
+      MONGO[:auction_values].insert_one(auction_values_doc)
+    end
+  end
+
+  class CurrentWeek
+    def self.get
+      FFNerdCache.get(:current_week)
+    end
+
+    def self.update
+      current_week_doc = {
+        dt: Time.now,
+        current_week: FFNerd.current_week
+      }
+
+      MONGO[:current_week].insert_one(current_week_doc)
+    end
+  end
+
+  class DraftRankings
+    def self.get
+      FFNerdCache.get(:draft_rankings)
+    end
+
+    def self.update
+      draft_rankings_doc = {
+        dt: Time.now,
+        players: FFNerd.standard_draft_rankings.map {|player| player.to_h}
+      }
+
+      MONGO[:draft_rankings].insert_one(draft_rankings_doc)
+    end
+  end
+
+  class DraftProjections
+    def self.get(query)
+      FFNerdCache.get(:draft_projections, query)
+    end
+
+    def self.update
+      POSITIONS.map do |position|
+        draft_projections_doc = {
+          dt: Time.now,
+          position: position,
+          players: FFNerd.draft_projections(position).map {|player| player.to_h}
+        }
+
+        MONGO[:draft_projections].insert_one(draft_projections_doc)
+      end
+    end
+  end
+
+  class WeeklyRankings
+    def self.get(query)
+      FFNerdCache.get(:weekly_rankings, query)
+    end
+
+    def self.update
+      WEEKS.map do |week|
+        POSITIONS.map do |position|
+          weekly_rankings_doc = {
+            dt: Time.now,
+            week: week,
+            position: position,
+            players: FFNerd.weekly_rankings(position, week).map {|player| player.to_h}
+          }
+
+          MONGO[:weekly_rankings].insert_one(weekly_rankings_doc)
+        end
+      end
+    end
+  end
+
+  class WeeklyProjections
+    def self.get(query)
+      FFNerdCache.get(:weekly_projections, query)
+    end
+
+    def self.update
+      WEEKS.map do |week|
+        POSITIONS_WITHOUT_DEF.map do |position|
+          weekly_projections_doc = {
+            dt: Time.now,
+            week: week,
+            position: position,
+            players: FFNerd.weekly_projections(position, week).map {|player| player.to_h}
+          }
+
+          MONGO[:weekly_projections].insert_one(weekly_projections_doc)
+        end
+      end
+    end
+  end
+end
