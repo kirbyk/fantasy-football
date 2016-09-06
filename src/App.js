5940620783950c82115d949e46c97858bd2609e6
ᴋɪʀʙʏ ᴋᴏʜʟᴍᴏʀɢᴇɴ <kirby@kkohlmorgen.com>
2016-09-05 22:51:31 -0400

    Remove log

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state.players);
     return (
       <div className="App">
         <Table className="table" data={this.state.players} itemsPerPage={25} 
